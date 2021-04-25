3e84472ae72929b4b7a8e2cb730684fd75c23a43
Md Jawad Hossain <jawadsgs3@gmail.com>
2021-04-25 13:30:15 +0600

    Update OwnLoop.py

--- OwnLoop.py
+++ OwnLoop.py
@@ -1,10 +1,14 @@
+import sys
+sys.setrecursionlimit(999999999)
+
 d = 1
 def lop(rt):
     global d
     a = 1
     if d <= rt:
-        print("Hello this is Md Jawad Hossain")  # your code here
+
+        print("Hello this is Md Jawad Hossain", d)  # your code here
         d += a
         lop(rt)
 
-lop(5) # how many time you want to run your code put it here
+lop(2462) # how many time you want to run your code put it here
